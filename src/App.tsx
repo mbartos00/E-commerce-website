12ca689eda75db01ceb7542c984e9932319f5c24
mbartos <maciekbartos8@gmail.com>
2023-03-31 14:09:38 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import GlobalStyle from './globalStyles';
+
+const App = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <h1>hello</h1>
+    </>
+  );
+};
+
+export default App;
---
+++ src/globalStyles.ts
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  *{
+    margin:0;
+    padding:0;
+    box-sizing:border-box;
+  }
+  body{
+    min-width:100vh;
+    font-family: 'Roboto', sans-serif;
+  }
+  h1,h2,h3{
+    font-weight:bold;
+  }
+  h1{
+    font-size: 3rem;
+  }
+  h2{
+    font-size: 1.75rem;
+  }
+  h3{
+    font-size: 1.125rem;
+
+  }
+`;
+export default GlobalStyle;
